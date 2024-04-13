88792cab8e6785a09c94f7bbf980f44d6f4ce959
jyx <jyxjyx1234@outlook.com>
2024-04-14 03:46:44 +0800

    修改

--- 译文/译文处理.py
+++ 译文/译文处理.py
@@ -3,22 +3,25 @@ import re
 
 with open('.\译文\WizAnniversary_transed.json','r',encoding='utf8') as f:
     yiwen=json.load(f)
+'''
+        "index"
+        "name": 
+        "pre_jp": 
+        "post_jp": 
+        "pre_zh": 
+        "proofread_zh":
+        "trans_by": 
+        "proofread_by": 
+        "post_zh_preview": 
+'''
 
 yiwen_processed=[]
-for dic in yiwen:
-    if '<汁>' not in dic["pre_jp"]:
-        if '<汗>' in dic["pre_jp"]:
-            if '<汁>' in dic["post_zh_preview"]:
-                dic["post_zh_preview"]=dic["post_zh_preview"].replace('<汁>','<汗>')
-    if '<汗>' not in dic["pre_jp"]:
-        if '<汁>' in dic["pre_jp"]:
-            if '<汗>' in dic["post_zh_preview"]:
-                dic["post_zh_preview"]=dic["post_zh_preview"].replace('<汗>','<汁>')
-    if not re.search('<.>',dic["pre_jp"]):
-        if re.search('<.>',dic["post_zh_preview"]):
-            dic["post_zh_preview"]=re.sub('<.>','',dic["post_zh_preview"])
-    
-    yiwen_processed.append(dic)
 
-with open('.\译文\WizAnniversary_transed_temp.json','w',encoding='utf8') as f:
-    json.dump(yiwen_processed, f , ensure_ascii=False, indent=4) 
+for dic in yiwen:
+    if re.search('<.>',dic["pre_jp"]):
+        if not re.search('<.>',dic["post_zh_preview"]):
+            print(dic["index"])
+
+
+#with open('.\译文\WizAnniversary_transed_temp.json','w',encoding='utf8') as f:
+#    json.dump(yiwen_processed, f , ensure_ascii=False, indent=4) 
